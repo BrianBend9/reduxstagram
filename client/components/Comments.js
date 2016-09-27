fd10c7bf5badcc678eb9055a2022a15e2d5b34c3
BrianBend9 <BrianBend9@users.noreply.github.com>
2016-09-27 15:00:38 -0400

    Setup render function for comments in Single component. Add comment form.

--- client/components/Comments.js
+++ client/components/Comments.js
@@ -1,10 +1,25 @@
 import React from 'react';
 
 const Comments = React.createClass({
+  renderComment(comment, i) {
+    <div>
+      <p>
+        <strong>{comment.user}</strong>
+        {comment.text}
+        <button className='remove-comment'>&times;</button>
+      </p>   
+    </div>
+  },
+
   render() {
     return (
       <div className='comment'>
-        I'm the comment!!!   
+        {this.props.postComments.map(this.renderComment)}
+        <form action='commentForm' className='comment-form'>
+          <input type='text' ref='author' placeholder='author' />
+          <input type='text' ref='comment' placeholder='comment' />
+          <input type='submit' hidden />
+        </form>
       </div>
     )
   }
