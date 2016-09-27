30fd5b92049590274a93707f531ef3fdd9d1efd2
BrianBend9 <BrianBend9@users.noreply.github.com>
2016-09-27 15:39:24 -0400

    Add return statement to render function. fix className typos.

--- client/components/Comments.js
+++ client/components/Comments.js
@@ -2,20 +2,22 @@ import React from 'react';
 
 const Comments = React.createClass({
   renderComment(comment, i) {
-    <div>
-      <p>
-        <strong>{comment.user}</strong>
-        {comment.text}
-        <button className='remove-comment'>&times;</button>
-      </p>   
-    </div>
+    return (
+      <div className='comment' key={i}>
+        <p>
+          <strong>{comment.user}</strong>
+          {comment.text}
+          <button className='remove-comment'>&times;</button>
+        </p>   
+      </div>
+    )
   },
 
   render() {
     return (
-      <div className='comment'>
+      <div className='comments'>
         {this.props.postComments.map(this.renderComment)}
-        <form action='commentForm' className='comment-form'>
+        <form ref='commentForm' className='comment-form'>
           <input type='text' ref='author' placeholder='author' />
           <input type='text' ref='comment' placeholder='comment' />
           <input type='submit' hidden />
